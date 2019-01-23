f1db5578540288677bfcbd2fe5f14941de046e38
Henrique Musseli Cezar <henrique.musseli@gmail.com>
2019-01-23 12:36:38 -0200

    Reading data from dfr.

--- dice2gromacs.py
+++ dice2gromacs.py
@@ -23,32 +23,134 @@ def extant_file(x):
         raise argparse.ArgumentTypeError("{0} does not exist".format(x))
     return x
 
-def check_parameters(dfrfile, txtfile):
+
+def read_bonds(fd):
+  line = fd.readline()
+
+  dfrBonds = {}
+  while "$end" not in line:
+    bnd = line.split()[0]+" "+line.split()[1]
+    if bnd not in dfrBonds:
+      dfrBonds[bnd] = line.split()[2:]
+    else:
+      dfrBonds[bnd].append(line.split()[2:])
+
+    line = fd.readline()
+
+  return dfrBonds
+
+
+def read_angles(fd):
+  line = fd.readline()
+
+  dfrAngles = {}
+  while "$end" not in line:
+    ang = line.split()[0]+" "+line.split()[1]+" "+line.split()[2]
+    if ang not in dfrAngles:
+      dfrAngles[ang] = line.split()[3:]
+    else:
+      dfrAngles[ang].append(line.split()[3:])
+
+    line = fd.readline()
+
+  return dfrAngles
+
+
+def read_dihedrals(fd):
+  line = fd.readline()
+
+  dfrDihedrals = {}
+  while "$end" not in line:
+    dih = line.split()[0]+" "+line.split()[1]+" "+line.split()[2]+" "+line.split()[3]
+    if dih not in dfrDihedrals:
+      dfrDihedrals[dih] = line.split()[4:]
+    else:
+      dfrDihedrals[dih].append(line.split()[4:])
+
+    line = fd.readline()
+
+  return dfrDihedrals
+
+
+def read_improper(fd):
+  line = fd.readline()
+
+  dfrImpDih = {}
+  while "$end" not in line:
+    idih = line.split()[0]+" "+line.split()[1]+" "+line.split()[2]+" "+line.split()[3]
+    if idih not in dfrImpDih:
+      dfrImpDih[idih] = line.split()[4:]
+    else:
+      dfrImpDih[idih].append(line.split()[4:])
+
+    line = fd.readline()
+
+  return dfrImpDih
+
+
+def read_dfr_dof(dfrfile):
+  with open(dfrfile, 'r') as f:
+    while True:
+      line = f.readline()
+
+      if not line:
+        break
+
+      if "$bond" in line:
+        dfrBonds = read_bonds(f)
+      elif "$angle" in line:
+        dfrAngles = read_angles(f)
+      elif "$dihedral" in line:
+        dfrDihedrals = read_dihedrals(f)
+      elif "$improper" in line:
+        dfrImpDih = read_improper(f)
+
+  return dfrBonds, dfrAngles, dfrDihedrals, dfrImpDih
+
+def read_txt_to_mol(txtfile):
   # convert the .txt to a .xyz to read in pybel mol to perceive all the bonds, angles, dihedrals..
   fd, temp_path = tempfile.mkstemp(suffix=".xyz")
   fxyz = os.fdopen(fd,'w')
   # table to convert atomic number to symbols
   etab = openbabel.OBElementTable()
+  
   with open(txtfile, 'r') as f:
     line = f.readline()
     combrule = line.strip()
+  
     line = f.readline()
     if int(line.strip()) != 1:
       print("Your .txt should have only one molecule, the one present in the .dfr")
       sys.exit(0)
+  
     line = f.readline()
     natoms = int(line.split()[0])
+    
     # write .xyz header
     fxyz.write("%d\nGenerated from %s\n" % (natoms,txtfile))
+    
     for i in range(natoms):
       line = f.readline()
       atnum = int(line.split()[1])
       x, y, z = [float(x) for x in line.split()[2:5]]
       fxyz.write("%s\t%f\t%f\t%f\n" % (etab.GetSymbol(atnum), x, y, z))
+  
   fxyz.close()
-  # read filel to a mol
+
+  # read filel to a mol, remove temp and return
   mol = pybel.readfile("xyz", temp_path).__next__()
   os.remove(temp_path)
+
+  return mol
+
+def check_parameters(dfrfile, txtfile):
+  # read the degrees of freedom from dfr into lists
+  dfrBonds, dfrAngles, dfrDihedrals, dfrImpDih = read_dfr_dof(dfrfile)
+  
+  # read the txt to a pybel mol object
+  mol = read_txt_to_mol(txtfile)
+
+
 
 
 if __name__ == '__main__':
